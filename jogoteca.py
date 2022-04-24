3ab1d1621a05328478555f3adb15ed3c79f8438a
RayssaNeder <rayssafreitas13@gmail.com>
2022-04-24 19:52:19 -0300

    Transformando lista de jogos em lista de objetos Jogo

--- jogoteca.py
+++ jogoteca.py
@@ -3,9 +3,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris', 'Super Mário', 'Pokemon Gold']
+    jogo1 = Jogo('Super Mário', 'Ação', 'SNES')
+    jogo2 = Jogo('Teris', 'Ação', 'SNES')
+    jogo3 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+    jogo4 = Jogo('Mortal Komat', 'Luta', 'SNES')
+
+    lista = [jogo1, jogo2, jogo3, jogo4 ]
     return render_template('lista.html', titulo='Games', jogos=lista )
 
 app.run()
